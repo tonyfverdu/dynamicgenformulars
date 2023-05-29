7e84db219de8158095d9db2eb22c91b4d8d52c6b
tonyfverdu <tony_verdu@hotmail.com>
2023-05-29 10:03:59 +0200

    commit ende

--- formdyngenerator/src/App.jsx
+++ formdyngenerator/src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import { BsXSquareFill } from 'react-icons/bs'
+import HeaderComponent from './components/HeaderComponent.jsx';
+import FormMain from './components/FormMain.jsx'
+import './sass/App.scss'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const title = 'MERN App Chat: React, MUI, NodeJS, Express and MongoDB';
+  const titleMemoListHeader = 'Dynamic Forms from JSON';
+  const subtitleMemoListHeader = 'MERN App (React-NodeJS-Express-MongoDB)';
+  const titleSite = 'A Application in React';
+
+  //Usestates variables: toggleHeader, 
+  const [toggleHeader, setToggleHeader] = useState(true);
+
+  //  Function for to exit of the header
+  function handleonClickExit(ev) {
+    setToggleHeader(!toggleHeader)
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="containerApp">
+      <HeaderComponent
+        title={titleMemoListHeader}
+        subtitle={subtitleMemoListHeader}
+      />
+      <div className="contCentral">
+        {toggleHeader &&
+          <>
+            <div className="containerLogos">
+              <figure className="MERNFigure">
+                <img className="imageLogo" src={`./src/assets/images/logos/React_logo.png`} alt='Logo React' />
+              </figure>
+              <figure className="MERNFigure">
+                <img className="imageLogo" src={`./src/assets/images/logos/logoMUI.png`} alt='Logo MUI' />
+              </figure>
+              <figure className="MERNFigure">
+                <img className="imageLogo" src={`./src/assets/images/logos/NodeJS.png`} alt='Logo NodeJS' />
+              </figure>
+              <figure className="MERNFigure">
+                <img className="imageLogo" src={`./src/assets/images/logos/nodeJSExpress.png`} alt='Logo Express' />
+              </figure>
+              <figure className="MERNFigure">
+                <img className="imageLogo" src={`./src/assets/images/logos/mongoDB.webp`} alt='Logo MongoDB' />
+              </figure>
+            </div>
+            <div className="header-Principal">
+              <h2>{title}</h2>
+            </div>
+          </>
+        }
+
+        <FormMain />
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="contIconExit" onClick={(ev) => handleonClickExit(ev)}>
+        <BsXSquareFill />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
--- formdyngenerator/src/main.jsx
+++ formdyngenerator/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './sass/index.css'
+import './sass/index.scss'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
